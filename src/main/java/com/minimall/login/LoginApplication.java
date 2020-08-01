851012ca38ad67518a7a763df60590072dfcf6da
mahmoud-Hasan <mahmoud.ahmed.hasan@outlook.com>
2020-08-01 20:40:38 +0200

    change config

--- src/main/java/com/minimall/login/LoginApplication.java
+++ src/main/java/com/minimall/login/LoginApplication.java
@@ -2,13 +2,21 @@ package com.minimall.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @SpringBootApplication
 public class LoginApplication {
 
+	private static final CharSequence secret = "HashSecret";	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
 	}
 	
-	
+	  @Bean("passwordEncoder")
+	    public PasswordEncoder passwordEncoder() {
+	        return new Pbkdf2PasswordEncoder(secret);
+    }
 }
--- src/main/java/com/minimall/login/config/AuthorizationServerConfig.java
+++ src/main/java/com/minimall/login/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -31,11 +31,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Order(1)
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
     
+	@Autowired
+	@Qualifier("passwordEncoder")
+    private PasswordEncoder passwordEncoder;
 	
     @Autowired
     private AuthenticationManager authenticationManager;
-    
-    private static final CharSequence secret = "HashSecret";	
 	
 	@Value("${exp:36000}")
 	private int expiration;
@@ -68,7 +69,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
     	clients.jdbc(dataSource);
    	
-    	System.out.println(" Password :: " + passwordEncoder().encode("password"));
+    	System.out.println(" Password :: " + passwordEncoder.encode("password"));
     
     }
 
@@ -82,10 +83,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         
         }
     
-    @Bean("passwordEncoder")
-    public PasswordEncoder passwordEncoder() {
-        return new Pbkdf2PasswordEncoder(secret);
-    }
+  
     
     @Bean
     public TokenEnhancer tokenEnhancer() {
